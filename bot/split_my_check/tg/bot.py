334a806758ced6b8b7828ff6f37a4c74d799b986
GeorgiySurkov <35404780+GeorgiySurkov@users.noreply.github.com>
2023-12-16 23:46:00 +0300

    implement expense group id generation

--- bot/split_my_check/tg/bot.py
+++ bot/split_my_check/tg/bot.py
@@ -2,6 +2,12 @@ import logging
 
 from aiogram import Router, types
 
+from split_my_check.utils import (
+    generate_expense_group_id,
+    prefix_expense_group_id,
+    parse_prefixed_expense_group_id,
+)
+
 # All handlers should be attached to the Router (or Dispatcher)
 bot_router = Router()
 
@@ -10,20 +16,46 @@ logger = logging.getLogger(__name__)
 
 @bot_router.inline_query()
 async def handle_inline_query(q: types.InlineQuery) -> None:
-    logger.info(f"Incoming inline request")
+    logger.info(f"Incoming inline request: {q.id=}, {q.query=}")
+    if len(q.query) > 64:
+        # Too long query
+        await q.answer(
+            results=[],
+            cache_time=0,
+            is_personal=True,
+        )
+        return
+
+    expense_group_id = generate_expense_group_id()
+    prefixed_expense_group_id = prefix_expense_group_id(expense_group_id)
+    logger.info(
+        f"Generated expense group id: {expense_group_id=}, {prefixed_expense_group_id=}"
+    )
+
     await q.answer(
         results=[
             types.InlineQueryResultArticle(
-                id="1",
+                id=prefixed_expense_group_id,
                 title="Создать трикаунт",
-                description="Будет отправлено сообщение, по которому можно будет зайти в нужный трикаунт",
+                description=f'Будет отправлено сообщение с ссылкой на новую группу расходов "{q.query}"',
                 input_message_content=types.InputTextMessageContent(
-                    message_text="А вот и трикаунт: https://t.me/splitmycheckbot/app?startapp=gr123",
+                    message_text=f"А вот и трикаунт: https://t.me/splitmycheckbot/app?startapp={prefixed_expense_group_id}",
                 ),
             )
         ],
         cache_time=0,
+        is_personal=True,
     )
+
+
+@bot_router.chosen_inline_result()
+async def handle_chosen_inline_result(q: types.ChosenInlineResult) -> None:
+    logger.info(f"Incoming chosen inline result: {q.result_id=}, {q.query=}")
+    expense_group_id = parse_prefixed_expense_group_id(q.result_id)
+    if expense_group_id is None:
+        logger.warning(f"Invalid expense group id: {q.result_id}")
+        return
+    logger.info(f"Expense group id: {expense_group_id}")
 
 
 # echo incoming message to bot
--- bot/split_my_check/utils.py
+++ bot/split_my_check/utils.py
@@ -1,7 +1,10 @@
+import re
 from contextlib import contextmanager
 from contextvars import ContextVar
 from datetime import datetime, timezone
 from typing import TypeVar, ContextManager
+
+from nanoid import generate
 
 T = TypeVar("T")
 
@@ -17,3 +20,22 @@ def set_context(context: ContextVar[T], value: T) -> ContextManager[None]:
 
 def now() -> datetime:
     return datetime.now(tz=timezone.utc)
+
+
+PREFIXED_EXPENSE_GROUP_ID_PATTERN = re.compile(r"^gr([0-9A-Za-z_-]{20})$")
+
+
+def generate_expense_group_id() -> str:
+    alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz-"
+    return generate(alphabet, size=20)
+
+
+def parse_prefixed_expense_group_id(expense_group_id: str) -> str | None:
+    match = PREFIXED_EXPENSE_GROUP_ID_PATTERN.match(expense_group_id)
+    if match is None:
+        return None
+    return match.group(1)
+
+
+def prefix_expense_group_id(expense_group_id: str) -> str:
+    return f"gr{expense_group_id}"
